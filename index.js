76a573e75d283d3e442fd87c6d272aa65b85030d
Mohit Agarwal <mohit724agarwal@gmail.com>
2022-09-30 00:07:46 +0530

    Multi News Links Service Added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -8,40 +8,73 @@ const app = express();
 const newsPapers = [
     {
         name: "theTimes",
-        address: "https://www.thetimes.co.uk/environment/climate-change"
+        address: "https://www.thetimes.co.uk/environment/climate-change",
+        base: ""
     },
     {
         name: "guardian",
-        address: "https://www.theguardian.com/environment/climate-crisis"
+        address: "https://www.theguardian.com/environment/climate-crisis",
+        base: ""
     },
     {
         name: "telegraph",
-        address: "https://www.telegraph.co.uk/climate-change/"
+        address: "https://www.telegraph.co.uk/climate-change/",
+        base: "https://www.telegraph.co.uk"
     }
 ]
 
 const articles = []
+
+newsPapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")',html).each(function (){
+                const title = $(this).text();
+                const url = $(this).attr('href');
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+        })
+})
 
 app.get('/', (req, res) => {
     res.json("Welcome to my climate change application")
 })
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-    .then((response) => {
-        const html = response.data;
-        const $ = cheerio.load(html)
+    res.json(articles)
+})
 
-        $('a:contains("climate")',html).each(function () {
-            const text = $(this).text()
+app.get('/news/:newspaperID', (req, res) => {
+    const newspaperID = req.params.newspaperID;
+
+    const newspaperAddress = newsPapers.filter(newspaper => newspaper.name == newspaperID)[0].address;
+    const newspaperBase = newsPapers.filter(newspaper => newspaper.name == newspaperID)[0].base;
+
+    axios.get(newspaperAddress)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const specificArticle = [];
+
+        $('a:contains("climate")', html).each(function(){
+            const title = $(this).text()
             const url = $(this).attr('href')
-            articles.push({
-                text,
-                url
+            specificArticle.push({
+                title,
+                url: newspaperBase + url,
+                source: newspaperID
             })
         })
-        res.json(articles)
-    })
-    .catch((err) => console.log(err))
+        res.json(specificArticle)
+    }).catch(err => console.log(err))
 })
+
 app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`))
